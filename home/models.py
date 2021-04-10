e28a8f9f5daf8c2b59cecdf1df7a315d67d057bd
AnuragGupta806 <54318429+AnuragGupta806@users.noreply.github.com>
2021-04-10 21:33:47 +0530

    Assignment models created

--- home/models.py
+++ home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Assignment(models.Model):
+    title = models.CharField(null=False,blank=False,max_length=500)
+    paper = models.FileField(upload_to="assignment/%Y/%m/%d/",null=True,blank=True)
+    remark = models.CharField(null=False,blank=False,max_length=500)
+    due_date = models.DateField(blank=False)
+    uploaded_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
+        
+class Submission(models.Model):
+    student = models.ForeignKey(User,on_delete=models.CASCADE)
+    paper = models.FileField(upload_to="assignment_submission/%Y/%m/%d/",blank=False)
+    uploaded_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
